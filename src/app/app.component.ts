ed3288e6d0468a814684bb83dded6a9fd4b3ed6b
Armani Appolon <ArmaniAppolon@gmail.com>
2022-07-11 23:24:39 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,35 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'todo-angular';
+  title = 'todo';
+
+  filter: 'all' | 'active' | 'done' = 'all';
+
+  allItems = [
+    { description: 'eat', done: true },
+    { description: 'sleep', done: false },
+    { description: 'play', done: false },
+    { description: 'laugh', done: true },
+  ];
+
+  get items() {
+    if (this.filter === 'all') {
+      return this.allItems;
+    }
+    return this.allItems.filter(item => this.filter === 'done' ? item.done : !item.done);
+  }
+
+  addItem(description: string) {
+    this.allItems.unshift({
+      description,
+      done: false
+    });
+  }
+
+  remove(item:any) {
+    this.allItems.splice(this.allItems.indexOf(item), 1);
+  }
+
 }
---
+++ src/app/item/item.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Input, Output, EventEmitter  } from '@angular/core';
+import { Item } from "../item";
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.css']
+})
+
+export class ItemComponent {
+
+  editable = false;
+
+  @Input() item: any;
+  @Input() newItem: any;
+  @Output() remove = new EventEmitter<Item>();
+
+  saveItem(description:string) {
+    if (!description) return;
+    this.editable = false;
+    this.item.description = description;
+  }
+}
+
